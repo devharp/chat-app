cffa822d14978ac09833f9a4dca75649c637e5d9
devharp <hsingh.161297@gmail.com>
2021-08-16 15:45:57 +0530

    did changes, created libs, copied progress.min.js in libs

--- web/script.js
+++ web/script.js
@@ -10,16 +10,38 @@ function keyu(e) {
         if (match[0] == user_input) {
             sessionid.style.borderColor = '#00a000';
         }
-        else{
-            sessionid.style.borderColor = '#f00';
+        else {
+            sessionid.style.borderColor = 'crimson';
         }
 
     }
     else {
         // Match not found
-        sessionid.style.borderColor = '#f00';
+        sessionid.style.borderColor = 'crimson';
 
     }
     sessionid.style.animation = 'key-down 0.3s infinite';
     setTimeout(function () { sessionid.style.animation = 'key-down 0.3s 0'; }, 100);
 }
+
+let join = {
+    e: document.querySelector('#join-session-btn'),
+    svg: document.querySelector('#join-session-btn svg'),
+    front: document.querySelector('#join-session-btn #front'),
+    back: document.querySelector('#join-session-btn svg #back'),
+    touch: document.getElementById('join-touch')
+};
+join.svg.style.top = String(join.svg.style.top - 2 - join.front.offsetTop) + 'px';
+join.svg.style.left = '0px';
+
+join.svg.setAttribute('width', String(join.e.clientWidth) + 'px');
+join.svg.setAttribute('height', String(join.e.clientHeight + 2) + 'px');
+
+console.log();
+function clickedJoin(event) {
+    let click = { x: event.offsetX, y: event.offsetY };
+    console.log(click);
+    
+}
+
+
