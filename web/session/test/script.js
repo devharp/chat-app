3cf4938801fafdd4750e0b22ab90d0b7b5b37bcc
devharp <hsingh.161297@gmail.com>
2021-08-26 18:02:00 +0530

    Added control and main screen section in session/test.html

--- web/session/test/script.js
+++ web/session/test/script.js
@@ -2,5 +2,6 @@ let win = { h: window.innerHeight, w: window.innerWidth };
 let container = { e: document.getElementById('container') };
 
 function init() {
-    container.e.style.height = `${win.h}px`;
+    // container.e.style.height = `${win.h}px`;
+    document.documentElement.style.setProperty('--win-h', `${win.h}px`);
 }
