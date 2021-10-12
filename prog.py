0e244287d212710419ada5605d49dcbf031da493
devharp <hsingh.161297@gmail.com>
2021-10-13 02:53:59 +0530

    Added Validation 1

--- prog.py
+++ prog.py
@@ -2,7 +2,20 @@ from flask_socketio import SocketIO, emit
 from flask import Flask, Response, request
 import random
 
-# from werkzeug.wrappers import request
+clients = []
+class Client:
+    node = None
+    session = None
+    temp = None
+    name = None
+    sid = None
+    connected = False
+    def __init__(self, id, sid) -> None:
+        self.node = id['node']
+        self.session = id['session']
+        self.temp = id['temp']
+        self.name = id['name']
+        self.sid = sid
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'password'
@@ -11,20 +24,43 @@ socketio = SocketIO(app)
 @socketio.on('id-validation')
 def handleIdValidation(payload):
     print('client sent for id validation: ' + str(payload))
-    id = {"node" : None, "session" : None, "temp" : None, "name" : None}
-    if payload['node'] == None or payload['node'] == 'null':
-        id["node"] = genRandomId(200)
-        print('node id na')
-    if payload['session'] == None or payload['session'] == 'null':
-        id["session"] = genRandomId(200)
-        print('session id na')
-    if payload['temp'] == None or payload['temp'] == 'null':
-        id["temp"] = genRandomId(200)
-        print('temp id na')
-    if payload['name'] == None or payload['name'] == 'null':
-        id["name"] = "John Doe"
-        print('name id na')
-    emit('id-received', id, sid=request.sid)
+    id = {'node' : None, 'session' : None, 'temp' : None, 'name' : None, 'sid' : request.sid}
+    if ((payload['node'] == None) or payload['node'] == 'null'):
+        payload['node'] = genRandomId(200)
+        print('Node Id Not Available')
+    else:
+        found = False
+        for x in range(len(clients)):
+            if clients[x].node == payload['node']:
+                found = True
+        if found == False:
+            payload['node'] = genRandomId(200)
+            
+    if ((payload['session'] == None) or payload['session'] == 'null'):
+        payload['session'] = genRandomId(200)
+        print('Session Id Not Available')
+    payload['temp'] = genRandomId(200)
+    if ((payload['name'] == None) or payload['name'] == 'null'):
+        payload['name'] = genRandomId(200)
+        print('Name Id Not Available')
+    
+    clients.append(Client(id=payload,sid=request.sid))
+    emit('id-received', payload, sid=request.sid)
+
+@socketio.on('disconnect')
+def socketDisconnected():
+    global clients
+    for x in range(len(clients)):
+        if str(clients[x].sid) == str(request.sid):
+            del clients[x]
+            print('Client ' + str((request.sid) + ' has been removed from the list'))
+    
+    print('clients size : ' + str(len(clients)))
+        
+
+@socketio.on('connect')
+def socketConnection():
+    print('Client ' + str(request.sid) + ' joined')
 
 @app.route('/')
 def home():
--- web/script.js
+++ web/script.js
@@ -2,54 +2,36 @@ let id;
 let storage = window.localStorage;
 
 function init() {
-    //Check IDs
-    checkIDS();
-}
-
-function checkIDS() {
-    id = { node: storage.getItem('node'), session: storage.getItem('session'), temp: storage.getItem('temp'), name: storage.getItem('name'), avail: true };
-    if (id.node == null) {
-        console.log('Node Id not found');
-        id.avail = false;
-    }
-    if (id.session == null) {
-        console.log('Session Id not found');
-        id.avail = false;
-    }
-    if (id.temp == null) {
-        console.log('Temp Id not found');
-        id.avail = false;
-    }
-    if (id.name == null) {
-        console.log('Name not found');
-        id.avail = false;
-    }
-    console.log('node id: ' + id.node);
-    console.log('session id: ' + id.session);
-    console.log('temp id: ' + id.temp);
-    console.log('name id: ' + id.name);
     socketConnection();
 }
 
 function socketConnection() {
+    id = {
+        node: window.localStorage.getItem('node'),
+        session: window.localStorage.getItem('session'),
+        temp: window.localStorage.getItem('temp'),
+        name: window.localStorage.getItem('name')
+    };
+
     var socket = io();
     socket.on('connect', function() {
-
-    });
-    if (!id.avail) {
         socket.emit('id-validation', {
             node: id.node,
             session: id.session,
             temp: id.temp,
             name: id.name
         });
-    }
+        console.log('sent: ');
+        console.log(id);
+
+    });
 
     socket.on('id-received', function(data) {
+        console.log('Received from the Web Server');
         console.log(data);
-        storage.setItem('node', data.node);
-        storage.setItem('session', data.session);
-        storage.setItem('temp', data.temp);
-        storage.setItem('name', data.name);
+        window.localStorage.setItem('node', data.node);
+        window.localStorage.setItem('session', data.session);
+        window.localStorage.setItem('temp', data.temp);
+        window.localStorage.setItem('name', data.name);
     });
 }
