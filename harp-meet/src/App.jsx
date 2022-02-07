19168bf1f4305121eccd6e3d22fcdaf93c02b2df
devharp <hsingh.161297@gmail.com>
2022-02-07 18:00:19 +0530

    Login page 'harp-meet' front working

--- harp-meet/src/App.jsx
+++ harp-meet/src/App.jsx
@@ -16,9 +16,14 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import TextField from '@mui/material/TextField';
 
 function App() {
-  const socket = socketClient('https://127.0.0.1:8000', { transports: ['websocket', 'polling', 'flashsocket'], secure: true });
+  // const socket = socketClient('https://127.0.0.1:8000', { transports: ['websocket', 'polling', 'flashsocket'], secure: true });
+  const socket = socketClient('https://localhost:8000', { transports: ['websocket'], secure: true });
 
   socket.on('connect', function () {
     console.log('client found server');
@@ -27,11 +32,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        {/* <Route path='home' element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path='home' element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route exactly path="/about" element={<About />} />
         <Route path="*" element={<NoPage />} />
 
       </Routes>
@@ -39,34 +42,110 @@ function App() {
   );
 }
 
-const Layout = () => {
-  return (
-    <>
-      <ResponsiveAppBar />
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <Outlet /> */}
-    </>
-  )
-};
-
 const Home = () => {
+  const [name, setName] = React.useState('');
+  const [name_status, setNameStatus] = React.useState(false);
+  const [firstnamehelper, setFirstNameHelper] = React.useState('Enter name here');
+
+  const [session, setSession] = React.useState('');
+  const [session_status, setSessionStatus] = React.useState(false);
+  const [firstsessionhelper, setFirstSessionHelper] = React.useState('Enter a Session ID or leave it empty');
+
+  React.useEffect(() => {
+    try {
+      if (name.length > 15) {
+        throw new Error('NameLimitCrossedError');
+      }
+      if (name.length !== 0) {
+        const validN = name.match(/^[A-Za-z][A-Za-z0-9 -]*$/);
+        if (validN === null) {
+          throw new Error('IncorrectNameError');
+        }
+        setFirstNameHelper('Go Ahead!');
+      }
+      else {
+        setFirstNameHelper('Enter name here');
+        setNameStatus(false);
+      }
+
+      if (session.length > 14) {
+        throw new Error('SessionLimitCrossedError');
+      }
+      if (session.length !== 0) {
+        const validN = session.match(/([a-f|0-9]{3,4})-([a-f|0-9]{3,4})-([a-f|0-9]{3,4})/);
+        if (validN === null) {
+          throw new Error('IncorrectSessionError');
+        }
+        setFirstSessionHelper('Go Ahead!');
+        setSessionStatus(false);
+      }
+      else {
+        setFirstSessionHelper('Enter a Session ID or leave it empty');
+        setSessionStatus(false);
+        console.log(session_status);
+      }
+
+
+    } catch (e) {
+      switch (e.message) {
+        case 'IncorrectNameError':
+          setNameStatus(true);
+          setFirstNameHelper('Invalid Name');
+          console.error('check your name: ', e.message);
+          break;
+        case 'IncorrectSessionError':
+          setSessionStatus(true);
+          setFirstSessionHelper('Invalid Session Format');
+          console.error('check your session: ', e.message);
+          break;
+        default:
+          console.error('Unknown Error occured: ', e.message);
+          break;
+      }
+    }
+  });
+
+
+  function onNameChangeHandler(event) {
+
+    setName(event.target.value);
+    // console.log(name);
+  }
+  function onSessionChangeHandler(event) {
+
+    setSession(event.target.value);
+    // console.log(name);
+  }
+
   return (
     <>
-      <NavBar />
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-between' }}>
+        <ResponsiveAppBar />
+        <Box mt={5} component="div" sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <Typography variant="h4" sx={{ display: 'flex', justifyContent: 'center', fontWeight: '500' }}>
+            Login
+          </Typography>;
+          <Box component="div" mt={3} >
+            <Box component="div" sx={{ display: 'flex', justifyContent: 'center' }}>
+              <TextField label="Your Name" inputProps={{ maxLength: 15 }} error={name_status} value={name} onChange={onNameChangeHandler} placeholder='Harpreet Singh' variant="outlined" size="small" />
+            </Box>
+            <FormHelperText sx={{ display: 'flex', justifyContent: 'center', color: (name_status) ? ('error.main') : ((name.length === 0) ? ('text.secondary') : ('primary.main')) }} id="my-helper-text">{firstnamehelper}</FormHelperText>
+          </Box>
+          <Box component="div" mt={3} >
+            <Box component="div" sx={{ display: 'flex', justifyContent: 'center' }}>
+              <TextField label="Session ID" inputProps={{ maxLength: 14 }} error={session_status} value={session} onChange={onSessionChangeHandler} placeholder='abc-123-a1c4' variant="outlined" size="small" />
+            </Box>
+            <FormHelperText sx={{ display: 'flex', justifyContent: 'center', color: (session_status) ? ('error.main') : ((session.length === 0) ? ('text.secondary') : ('primary.main')) }} id="my-helper-text">{firstsessionhelper}</FormHelperText>
+          </Box>
+          <Box mt={3} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Button variant="contained" >Begin</Button>
+          </Box>
+        </Box>
+
+        <AppBar position='relative' sx={{ mt: 2 }}>
+          <Container sx={{ p: 1 }} ><div style={{ display: 'flex', justifyContent: 'center' }}>&copy; HarpMeet 2021</div></Container>
+        </AppBar>
+      </div>
     </>
   );
 };
@@ -76,32 +155,31 @@ const About = () => {
   for (const [key, value] of entries) {
     console.log('key: ' + key + '\tvalue: ' + value);
   }
-  return <h1>Blog Articles</h1>;
-};
+  return (
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-between' }}>
+        <ResponsiveAppBar />
+        <div style={{ flexGrow: '1' }}>About page</div>
 
-const Contact = () => {
-  return <h1>Contact Me</h1>;
+        <AppBar position='relative'>
+          <Container sx={{ p: 1 }} ><div style={{ display: 'flex', justifyContent: 'center' }}>Hello</div></Container>
+        </AppBar>
+      </div>
+    </>);
 };
 
 const NoPage = () => {
   return (
     <>
+      <ResponsiveAppBar />
       Error Page 404
     </>
   );
 };
 
-const NavBar = () => {
-  return (
-    <>
-      NavBar
-    </>
-  );
-}
-const pages = ['home', 'about'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 const ResponsiveAppBar = () => {
+  const pages = ['home', 'about'];
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -112,8 +190,9 @@ const ResponsiveAppBar = () => {
     setAnchorElUser(event.currentTarget);
   };
 
-  function navClicked(i){
-    console.log('item no: ', i , ' clicked');
+  function navClicked(i) {
+    //Useless function, yet
+    console.log('item no: ', i, ' clicked');
     handleCloseNavMenu()
   }
 
@@ -135,7 +214,7 @@ const ResponsiveAppBar = () => {
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-            LOGO
+            HarpMeet
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -169,8 +248,8 @@ const ResponsiveAppBar = () => {
             >
               {pages.map((page, i) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" component={Button} to={page} sx={{color: 'black', textDecoration: 'none'}}>
-                    {page}
+                  <Typography textAlign="center" component={Button} sx={{ color: 'black', textDecoration: 'none' }}>
+                    <Link to={`/${page}`} style={{ color: 'black', textDecoration: 'none' }}>{page}</Link>
                   </Typography>
                 </MenuItem>
               ))}
@@ -182,16 +261,16 @@ const ResponsiveAppBar = () => {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
-            LOGO
+            HarpMeet
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page, i) => (
               <Button
                 key={page}
-                onClick={() =>{navClicked(i)}}
+                onClick={() => { navClicked(i) }}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                <Link to={`${page}`} style={{color: 'white', textDecoration: 'none'}}>{page}</Link>
+                <Link to={`/${page}`} style={{ color: 'white', textDecoration: 'none' }}>{page}</Link>
               </Button>
             ))}
           </Box>
